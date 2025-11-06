87bd1680a0231de4cf50c5b1c0c03866098becb5
NesrineYAH <nesrineyahoum@gmail.com>
2025-11-07 00:10:16 +0100

    Mise à jours

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -21,7 +21,45 @@ export default function Footer() {
         <a href={`../MentionLegales/#mentionsLegales`} target="_blank" rel="noopener noreferrer" className='TitreH3'>Mentiens Legales</a>
         </h3> 
       </div>
-  
+    <ul className='footer__ul'>
+        <li>
+            <a href='' target="_blank" rel="noopener noreferrer" className='TitreH3'>Nouveauté parfum</a>
+        </li>   
+         <li>
+        <a href={`../MentionLegales/#mentionsLegales`} target="_blank" rel="noopener noreferrer" className='TitreH3'>Soin visage</a>
+        </li> 
+        <li>
+        <a href={`../MentionLegales/#mentionsLegales`} target="_blank" rel="noopener noreferrer" className='TitreH3'>Nos magasins</a>
+        </li> 
+        <li>
+        <a href={`../MentionLegales/#mentionsLegales`} target="_blank" rel="noopener noreferrer" className='TitreH3'>Guide d'achats beauté</a>
+        </li> 
+          <li>
+        <a href={`../MentionLegales/#mentionsLegales`} target="_blank" rel="noopener noreferrer" className='TitreH3'>Guide parfums</a>
+        </li> 
+      </ul>
+          <ul className='footer__ul'>Conditions
+            <li>Politique Cookies</li>
+            <li>Mentions légales</li>
+            <li>Politique Cookies</li>
+            <li>CGV</li>
+            <li>RGBP</li>
+            <li>RSGP</li>
+          </ul>
+               <ul className='footer__ul'>Suivez-nous Aide
+        <ul>
+					<li>
+									<a href="https://www.instagram.com/" title="instagram" target="_blank" rel="noreferrer noopener" class="">
+						<i class="icon-instagram"></i>
+					</a>
+							</li>
+					<li>
+									<a href="https://www.facebook.com/" title="facebook" target="_blank" rel="noreferrer noopener" class="">
+						<i class="icon-facebook"></i>
+					</a>
+							</li>
+			</ul>
+          </ul>
       <a className="fa" title="Go to top" href="/home">
       <FaChevronUp id="toTop "/>
       </a>
