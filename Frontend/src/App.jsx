c9f38ccb109122189ed42e1e55e2029fe7cf0b12
NesrineYAH <nesrineyahoum@gmail.com>
2025-11-20 13:59:53 +0100

    ADD INFOSITE CONPONENT

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.scss";
 import Headers from "./components/Header/Header";
-import  Home  from "./pages/Home/Home";
-import { Routes, Route } from 'react-router-dom';
-import Product from './pages/Product/Product';
+import Home from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import Product from "./pages/Product/Product";
 import Footer from "./components/Footer/Footer";
- import BlogBenefits from './components/Blog/BlogBenefits';
-import  Newsletter from './components/Newsletter/Newsletter';
-import  CookieModel from './components/CookieModel/CookieModel';
-import InfoSite from './components/InfoSite/InfoSite';
+import BlogBenefits from "./components/Blog/BlogBenefits";
+import Newsletter from "./components/Newsletter/Newsletter";
+import CookieModel from "./components/CookieModel/CookieModel";
+import InfoSite from "./components/InfoSite/InfosSite";
 import Favorites from "./pages/Favorites/Favorites";
-import PolitiqueCookies from  "./pages/PolitiqueCookies/PolitiqueCookies";
+import PolitiqueCookies from "./pages/PolitiqueCookies/PolitiqueCookies";
 import AdminAddProduct from "./pages/admin/add-product";
 import Confientilaite from "./pages/CONFI/Confidentialite";
 import "./App.scss";
@@ -18,34 +18,31 @@ import Profil from "./pages/Profil/Profil";
 import Cart from "./pages/Cart/Cart";
 
 function App() {
-
   return (
     <>
       <div className="App">
-             <Headers />
+        <Headers />
         <h1 id="title">Parfume-Algarve-shop</h1>
-     
+
         <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/product/:id" element={<Product />} /> {/* Remarque : :id */}
-         <Route path="/admin/add-product" element={<AdminAddProduct />} />
-         <Route path="/Favorites" element={<Favorites />} />
-         <Route path="/PolitiqueCookies" element={<PolitiqueCookies />} />
-         <Route path="/Confientilaite" element={<Confientilaite />} />
-         <Route path="/Authentification" element={<Authentification />} />
-         <Route path="/Profil" element={<Profil />} />
-         <Route path="/Cart" element={<Cart />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<Product />} />{" "}
+          {/* Remarque : :id */}
+          <Route path="/admin/add-product" element={<AdminAddProduct />} />
+          <Route path="/Favorites" element={<Favorites />} />
+          <Route path="/PolitiqueCookies" element={<PolitiqueCookies />} />
+          <Route path="/Confientilaite" element={<Confientilaite />} />
+          <Route path="/Authentification" element={<Authentification />} />
+          <Route path="/Profil" element={<Profil />} />
+          <Route path="/Cart" element={<Cart />} />
+        </Routes>
 
-      </Routes>
-     
-      <BlogBenefits />
-         <Newsletter />
-         <CookieModel />
-         <InfoSite />
-  
-
+        <BlogBenefits />
+        <Newsletter />
+        <CookieModel />
+        <InfoSite />
       </div>
-           <Footer />
+      <Footer />
     </>
   );
 }
