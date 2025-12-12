113ec886cf21734b144ffaf9078b28459d0d9f08
NesrineYAH <nesrineyahoum@gmail.com>
2025-12-12 22:42:39 +0100

    Mise en place l'internalisation

--- Frontend/src/pages/FAQ/FAQ.jsx
+++ Frontend/src/pages/FAQ/FAQ.jsx
@@ -2,26 +2,31 @@ import React, { useState } from "react";
 import "./FAQ.scss";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import fr from "../../Lang/fr.json"; // importe ton fichier JSON
+import { useTranslation } from "react-i18next";
 
 export default function FAQ() {
   const [openIndex, setOpenIndex] = useState(null);
-
+  const { t } = useTranslation();
   const togglePref = (index) => {
     setOpenIndex(openIndex === index ? null : index);
   };
+const paymentFaq = t("faq.payment", { returnObjects: true });
+  const ordersFaq = t("faq.orders", { returnObjects: true });
+  const deliveryFaq = t("faq.delivery", { returnObjects: true });
+  const returnsFaq = t("faq.returns", { returnObjects: true });
 
   return (
     <section id="Faq">
-      <div className="cookie-page">
-        <h1>Foire Aux Questions</h1>
+      <div className="Faq-page">
+          <h1>{t("faq.PrincipalTitle")}</h1>
         <p>
           Cette page explique comment notre site utilise les cookies pour
           améliorer votre expérience et assurer le bon fonctionnement du
           service.
         </p>
-        <h2>Paiement</h2>
-        <div className="consent-section">
-          {fr.faqDataP.map((item, index) => (
+         <h2>{t("faq.PaymentTitle")}</h2>
+        <div className="Faq-section">
+          {paymentFaq.map((item, index) => (
             <article className="option" key={index}>
               <div className="option__div" onClick={() => togglePref(index)}>
                 {openIndex === index ? (
@@ -41,9 +46,9 @@ export default function FAQ() {
           ))}
         </div>
         {/** */}
-        <h2>Commandes</h2>
+        <h2>{t("faq.OrdersTitle")}</h2>
         <div className="consent-section">
-          {fr.faqDataC.map((item, index) => (
+          {ordersFaq.map((item, index) => (
             <article className="option" key={index}>
               <div className="option__div" onClick={() => togglePref(index)}>
                 {openIndex === index ? (
@@ -62,9 +67,9 @@ export default function FAQ() {
             </article>
           ))}
         </div>
-        <h2>Livraison</h2>
+         <h2>{t("faq.DeliveryTitle")}</h2>
         <div className="consent-section">
-          {fr.faqDataL.map((item, index) => (
+          {deliveryFaq.map((item, index) => (
             <article className="option" key={index}>
               <div className="option__div" onClick={() => togglePref(index)}>
                 {openIndex === index ? (
@@ -83,9 +88,9 @@ export default function FAQ() {
             </article>
           ))}
         </div>
-        <h2>Retours et reboursement </h2>
+        <h2>{t('faq.ReturnsTitle')}</h2>
         <div className="consent-section">
-          {fr.faqDataR.map((item, index) => (
+          {returnsFaq.map((item, index) => (
             <article className="option" key={index}>
               <div className="option__div" onClick={() => togglePref(index)}>
                 {openIndex === index ? (
